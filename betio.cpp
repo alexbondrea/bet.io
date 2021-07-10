8fab1e43fd4d63d64aa8c05c2f53767400bca2f3
Alexandru-Nadav Bondrea <47431790+alexbondrea@users.noreply.github.com>
2021-07-10 18:03:24 +0300

    Update betio.cpp
    -Permite reținerea în fișier a mai multor combinații de pariuri;
    -Calculează și reține corect valorile (în considerentul minimizării timpului general de execuție 1/2 (reduced backtracking values))

--- betio.cpp
+++ betio.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-//v 1.25 *ab*
-int pariu_sportiv_in_trei(float cota_a, float cota_b, float cota_egal, float sum, float &val_a, float &val_egal, float &val_b, float &min_win, float &max_win) {
+//v 1.3 *ab*
+void pariu_sportiv_in_trei(float cota_a, float cota_b, float cota_egal, float sum, float &val_a, float &val_egal, float &val_b, float &min_win, float &max_win) {
     short op = 0;
     for (float i = 10; i>0; i=i-0.1) {
         for (float j = 10-i; j>0; j=j-0.1) {
             float f = 10 - i - j;
             if (i+j+f == 10 && i*cota_egal > 10 && j*cota_a > 10 && f*cota_b > 10) {
-                float max = 0, min = 0;
                 float a = i * cota_egal;
                 float b = j * cota_a;
                 float c = f * cota_b;
@@ -50,7 +49,7 @@ int pariu_sportiv_in_trei(float cota_a, float cota_b, float cota_egal, float sum
     }
 }
 
-int pariu_sportiv_simplu(float cota_a, float cota_b, float sum, float &val_a, float &val_b, float &min_win, float &max_win) {
+void pariu_sportiv_simplu(float cota_a, float cota_b, float sum, float &val_a, float &val_b, float &min_win, float &max_win) {
     short op = 0;
     for (float i = 10; i>0; i=i-0.1) {
         float j = 10 - i;
@@ -74,7 +73,7 @@ int pariu_sportiv_simplu(float cota_a, float cota_b, float sum, float &val_a, fl
     if (op==0 || min_win < 0.5) cout << "Not worth it man" << endl;
     else if (op==1) {
         cout << "Bet rentabil după cum urmeaza:" << endl;
-        cout << "Pe echipa A: " << sum / 10 * val_a<< endl;
+        cout << "Pe echipa A: " << sum / 10 * val_a << endl;
         cout << "Pe echipa B: " << sum / 10 * val_b << endl;
         cout << "Castig maxim: " << sum / 10 * max_win << endl;
         cout << "Castig minim: " << sum / 10 * min_win << endl;
@@ -97,13 +96,14 @@ int main () {
         else pariu_sportiv_simplu(cota_a, cota_b, sum, val_a, val_b, min_win, max_win);
         cout << "Salvezi aceste informatii? (y/n) "; cin >> option; cout << endl;
         if (option=='y'||option=='Y') {
-            ofstream file_output_information("Pariu_Sportiv.txt");
+            fstream file_output_information;
+            file_output_information.open("Pariu_Sportiv.txt", fstream::app);
             file_output_information << "Informatii despre pariu:" << endl;
-            file_output_information << "Cota 1: " << cota_a << " -> valoare: " << val_a << endl;
-            if (cota_egal!=-1) file_output_information << "Cota egal: " << cota_egal << " -> valoare: " << val_egal << endl;
-            file_output_information << "Cota 2: " << cota_b << " -> valoare: " << val_b << endl;
-            file_output_information << "Castig maxim: " << max_win << endl;
-            file_output_information << "Castig minim: " << min_win << endl;
+            file_output_information << "Cota 1: " << cota_a << " -> valoare: " << sum / 10 * val_a << endl;
+            if (cota_egal!=-1) file_output_information << "Cota egal: " << cota_egal << " -> valoare: " << sum / 10 * val_egal << endl;
+            file_output_information << "Cota 2: " << cota_b << " -> valoare: " << sum / 10 * val_b << endl;
+            file_output_information << "Castig maxim: " << sum / 10 * max_win << endl;
+            file_output_information << "Castig minim: " << sum / 10 * min_win << endl;
             file_output_information.close();
             cout << "Salvat!" << endl << endl;
         }
